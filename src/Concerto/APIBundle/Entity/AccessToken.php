3a44d6d457d89acad22a424f873863c566ed3a1e
j-pillon <julienpillon@gmail.com>
2025-04-10 14:55:11 +0200

    fix: set inversedBy property for relation with Client

--- src/Concerto/APIBundle/Entity/AccessToken.php
+++ src/Concerto/APIBundle/Entity/AccessToken.php
@@ -24,7 +24,7 @@ class AccessToken extends BaseAccessToken
     protected $id;
     
     /**
-     * @ORM\ManyToOne(targetEntity="Client")
+     * @ORM\ManyToOne(targetEntity="Client", inversedBy="accessTokens")
      * @ORM\JoinColumn(nullable=false)
      */
     protected $client;
